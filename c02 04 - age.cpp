3968ec2bad42a0d4f3204aa89bccb6cc0567b784
Michelle Zulli <mzchellz@gmail.com>
2015-05-22 07:41:37 -0400

    First commit

---
+++ c02 04 - age.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+main() {
+	int yob, age;
+	cout << "ENTER YOUR YEAR OF BIRTH: ";
+	cin >> yob;
+	age = 2025 - yob;
+	cout << "Your age would be " << age << " by the year 2025.\n";
+	return 0;
+}//MAIN
